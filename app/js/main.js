af51ff776e5c8c1e0a39d44b8473a2740643177e
oleandra <ollleandra@gmail.com>
2017-09-06 10:30:48 +0300

    app\ added

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#fullpage').fullpage({
+        verticalCentered: true,
+        navigation:true,
+        sectionSelector: 'section,.section',
+        // resize: false,
+        anchors: ['firstScreen', 'what', 'project', 'contact', 'reviews', 'footer']
+    });
+});
+
