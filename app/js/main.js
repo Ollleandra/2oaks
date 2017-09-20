2276d90f7c8e05fcd3357b2b350bb0facbda21ff
oleandra <ollleandra@gmail.com>
2017-09-20 23:50:28 +0300

    media added

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
-    $('#fullpage').fullpage({
-        verticalCentered: true,
-        navigation:true,
-        sectionSelector: 'section',
-        anchors: ['firstScreen', 'what', 'project', 'contact', 'reviews']
+    $(".hamburger").click(function(){
+        $(this).toggleClass("is-active");
+    });
+    function createFullPage() {
+        if($(window).width() > 1024) {
+            $("#fullpage").fullpage(
+                {
+                    verticalCentered: true,
+                    navigation: true,
+                    sectionSelector: 'section',
+                    anchors: ['section0', 'what', 'project', 'contact', 'reviews']
+                }
+            );
+        }
+    }
+    $(function() {
+        createFullPage();
+    });
+    $(window).resize(function(e) {
+        if( $(window).width() > 1024) {
+            createFullPage();
+        } else {
+            $.fn.fullpage.destroy("all");
+        }
     });
 });
 
